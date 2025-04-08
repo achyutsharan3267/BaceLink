40e08cad8f2080e6bdc9c2a98171376ad53d0670
achyutsharan3267 <achyut.sharan@impressico.com>
2025-04-09 00:00:20 +0530

    data added

--- src/Cards.tsx
+++ src/Cards.tsx
@@ -1,18 +1,26 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { group } from './Data';
 
 function Cards() {
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
+      {
+        group.map((item , index)=>(
+          <>
+          <Card.Img variant="top" src={item.imageUrl} />
+          <Card.Body>
+            <Card.Title>{item.name}</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary" href={item.link}>join</Button>
+          </Card.Body>
+          </>
+        ))
+      }
+    
     </Card>
   );
 }
---
+++ src/Data.ts
@@ -0,0 +1,17 @@
+export const group: { name: string, link: string, imageUrl: string }[] = [
+  {
+    "name": "On the Way to Krishna",
+    "link": "https://chat.whatsapp.com/KPDzSSgfxTj6pxrKVuCBlk",
+    "imageUrl": "https://rukminim3.flixcart.com/image/850/1000/jevpj0w0/book/7/0/9/on-the-way-to-krsna-original-imaf2khqqpbhajmq.jpeg?q=90&crop=false"
+  },
+  {
+    "name": "Krishna Ki Aur",
+    "link": "https://chat.whatsapp.com/D1baS5hDVq53FTaZzkI0HF",
+    "imageUrl": "https://m.media-amazon.com/images/I/51S5kJfO9kL._AC_UF1000,1000_QL80_.jpg"
+  },
+  {
+    "name": "Raja Vidya",
+    "link": "https://chat.whatsapp.com/GMASWIl0vT40UgCU2dESwM",
+    "imageUrl": "https://cdn.quicksell.co/-MlBEkvUoxGbM94EUdk7/products/-Mn4vp1a5TAWFx3SJ_K0.jpg"
+  }
+];
