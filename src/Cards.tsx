d35c7ef95fe014c0e0f0ca45a429f12e3477ee0c
achyutsharan3267 <achyut.sharan@impressico.com>
2025-04-08 23:46:33 +0530

    card

---
+++ src/Cards.tsx
@@ -0,0 +1,20 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function Cards() {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Cards;
