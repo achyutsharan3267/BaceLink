a7de85c7fb56cc6ec3a7156f1628be748effb38a
achyutsharan3267 <achyut.sharan@impressico.com>
2025-04-09 22:59:20 +0530

    text added

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import Cards from "./Cards"
 const App = () => {
   return (
     <div>
-      <h1 className="text-white text-center mb-5">MayaPur Bace </h1>
-      <h2 className="text-white mb-5">"The ultimate goal of life is to develop a loving relationship with God, Krishna. Everything else is temporary. By dedicating ourselves to this goal, we can find true peace, happiness, and fulfillment in this world and the next."</h2>
+      <h1 className="text-white text-center mb-5">Bace </h1>
+      <h2 className="text-white mb-5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias non temporibus id ut. A at distinctio quas laboriosam ipsam deleniti earum labore optio neque, quia dolore voluptatem suscipit corporis vitae repellat atque ratione fuga quae aspernatur veritatis impedit sunt error. Molestiae quidem dolorum labore fugit temporibus ratione adipisci commodi saepe at veritatis quae odio iusto quod fuga hic recusandae explicabo corrupti dicta modi, nam ipsa quisquam. Voluptatibus dolorem ut consequuntur quasi. Doloremque possimus temporibus cumque reprehenderit modi quos minus, magnam velit corrupti, nobis nisi nostrum deleniti tenetur distinctio expedita iure blanditiis laboriosam. At dicta praesentium molestiae? Similique aspernatur architecto deleniti exercitationem est eaque accusantium, optio odio possimus magnam reprehenderit voluptates libero ea. Ut natus dolorum nihil iure quod totam! Possimus!</h2>
       <div className="text-center">
 
         <img className="w-100" src="https://cdn.tovp.org/wp-content/uploads/2024/01/srila-prabhupada-walking-w-shadow-web.png" alt="" />
