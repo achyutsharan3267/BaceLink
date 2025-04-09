842697fe9aefe9ad403a5685275df9fa7ffc7810
achyutsharan3267 <achyut.sharan@impressico.com>
2025-04-09 10:43:24 +0530

    prabhupad image added

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,13 @@ const App = () => {
   return (
     <div>
       <h1 className="text-white text-center mb-5">MayaPur Bace </h1>
-      <Cards/>
+      <h2 className="text-white mb-5">"The ultimate goal of life is to develop a loving relationship with God, Krishna. Everything else is temporary. By dedicating ourselves to this goal, we can find true peace, happiness, and fulfillment in this world and the next."</h2>
+      <div className="text-center">
+
+        <img className="w-100" src="https://cdn.tovp.org/wp-content/uploads/2024/01/srila-prabhupada-walking-w-shadow-web.png" alt="" />
+      </div>
+      <Cards />
+
     </div>
   )
 }
