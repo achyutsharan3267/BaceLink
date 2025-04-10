e9890c4d0c7bf7cec703e812e9ef6f5d21fb0792
achyutsharan3267 <achyut.sharan@impressico.com>
2025-04-11 00:16:20 +0530

    inital commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div>
       <h1 className="text-white text-center mb-5">Bace </h1>
-      <h2 className="text-white mb-5">Lorem ipsum dolor,</h2>
+      <h2 className="text-white mb-5">Lorem ipsum dolor, loremssk</h2>
       <div className="text-center">
 
         <img className="w-100" src="https://cdn.tovp.org/wp-content/uploads/2024/01/srila-prabhupada-walking-w-shadow-web.png" alt="" />
