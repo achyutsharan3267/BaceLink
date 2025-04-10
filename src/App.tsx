a2a469bc10b860768081aabb0795c780e1b92a5d
achyutsharan3267 <achyut.sharan@impressico.com>
2025-04-10 23:59:05 +0530

    shorten subtitle text in App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div>
       <h1 className="text-white text-center mb-5">Bace </h1>
-      <h2 className="text-white mb-5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias non temporibus id ut. A at distinctio quas laboriosam ipsam deleniti earum labore optio neque, quia dolore voluptatem suscipit corporis vitae repellat atque ratione fuga quae aspernatur veritatis impedit sunt error. Molestiae quidem dolorum labore fugit temporibus ratione adipisci commodi saepe at veritatis quae odio iusto quod fuga hic recusandae explicabo corrupti dicta modi, nam ipsa quisquam. Voluptatibus dolorem ut consequuntur quasi. Doloremque possimus temporibus cumque reprehenderit modi quos minus, magnam velit corrupti, nobis nisi nostrum deleniti tenetur distinctio expedita iure blanditiis laboriosam. At dicta praesentium molestiae? Similique aspernatur architecto deleniti exercitationem est eaque accusantium, optio odio possimus magnam reprehenderit voluptates libero ea. Ut natus dolorum nihil iure quod totam! Possimus!</h2>
+      <h2 className="text-white mb-5">Lorem ipsum dolor,</h2>
       <div className="text-center">
 
         <img className="w-100" src="https://cdn.tovp.org/wp-content/uploads/2024/01/srila-prabhupada-walking-w-shadow-web.png" alt="" />
